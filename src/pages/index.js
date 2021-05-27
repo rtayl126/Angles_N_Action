52db2848c66f4e3ab97a70a5f686d31c06219ac8
rtayl126 <74155680+rtayl126@users.noreply.github.com>
2021-05-26 22:24:53 -0400

    info

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,21 +8,27 @@ import Seo from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <Seo title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <StaticImage
-      src="../images/gatsby-astronaut.png"
-      width={300}
-      quality={95}
-      formats={["AUTO", "WEBP", "AVIF"]}
-      alt="A Gatsby astronaut"
-      style={{ marginBottom: `1.45rem` }}
-    />
-    <p>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-    </p>
+    <h1>Home page</h1>
+    <p1>Who we are: Angel N Action Child Visitation Services is a company who is interested in the welfare of children and parents rights,who are seeking professional child visitation services.</p1>
+    <h1>About us</h1>
+    <p1>here at angel n action child visitation services, my goal is to provide children the opportunity to visit their non-custodial parents in an enjoyable, safe visit, stress free, and in a 
+relaxing environment.</p1>
+    <h1>professional visitation monitor</h1>
+    <p1>Family code section 3200 defines the term "provider' as including any individual or supervised visitation center that monitors visitations. supervised visitation contacts between a non custodial party one or more children in the presence of a neutral third party.</p1>
+    <h1>Our Services</h1>
+    <p1>all court cases domestic violence sexual abuse etc. please call about this one.
+ counties: orange county, san bernardino, and riverside </p1>
+    <h1>Fee Schedule</h1>
+    <p1>Off site supervised visitation                         call for pricing
+custody exchange monitoring                       call for pricing
+skype monitoring                                           call for pricing
+intake fee                                                       call for pricing
+reports for attorneys, parents, or court           call for pricing
+reports, and miscellaneous services               call for pricing</p1>
+<h1>Contact Us</h1>
+<p1>please contact Shea Lynum at 562 5443232  
+office hours: 7:00am-5:00pm Monday-Friday 
+                     9:00am-3:00pm Saturday</p1>
   </Layout>
 )
 
