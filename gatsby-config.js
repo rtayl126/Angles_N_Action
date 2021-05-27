3c583cbb3eaefcdb356e6edf91ebb599447b9fab
rtayl126 <74155680+rtayl126@users.noreply.github.com>
2021-05-26 22:05:23 -0400

    angles naction

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,27 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Angles-N-Action`,
+    menuLinks:[
+      +   { name:'Home',
+            link:'/'
+          },
+          { name:'About Us',
+            link:'/'
+          },
+          { name:'home',
+            link:'/'
+          },
+          { name:'home',
+            link:'/'
+          },
+          { name:'home',
+            link:'/'
+          },
+          { name:'home',
+            link:'/'
+          },
+          
+     ]
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
